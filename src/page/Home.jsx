c7439eb95f74dd3d9edeb0edd45889ced950abb2
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-02 18:17:39 +0300

    add isOpen and text states

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,13 @@
 import AddTask from "../components/addTask/AddTask"
 import TaskList from "../components/taskList/TaskList"
 import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
 const Home = () => {
+
+const [isOpen, setIsOpen] = useState(false);
+const [text, setText] = useState("Show Task Bar");
+
   return (
     <div>
         <Button variant="danger">Show Task Bar</Button>
