29f8c1db7dc94c2260283b01e32d8a1fa130082d
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-03 00:02:15 +0300

    FiDelete icons css added

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,24 +1,34 @@
-import {FiDelete} from "react-icons/fi"
+import { FiDelete } from "react-icons/fi";
 
 const TaskList = ({ task }) => {
   return (
     <div>
-        {task.map((item) => {
-            const {id, task, date} = item;
-            return(
-                <div className="d-flex justify-content-between mt-2 bg-secondary rounded-3 p-3" key = {id}>
-                    <div>
-                        <h4>{task}</h4>
-                        <p>{date}</p>
-                    </div>
-                    <div>
-                        <FiDelete/>
-                    </div>
-                </div>                
-            )
-        })}
+      {task.map((item) => {
+        const { id, task, date } = item;
+        return (
+          <div
+            className="d-flex justify-content-between mt-2 bg-secondary rounded-3 p-3"
+            key={id}
+          >
+            <div>
+              <h4>{task}</h4>
+              <p>{date}</p>
+            </div>
+            <div>
+              <FiDelete 
+              style = {{
+                cursor:"pointer",
+                marginRight:"20px",
+                fontSize:"2rem",
+                boxShadow:"2px 2px 2px white"
+              }}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
