113d617ecf5ce89c2649ab580ec7d514d4b14dd0
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-03 00:53:31 +0300

    form added

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,9 +1,24 @@
-
+import { Button, Form } from "react-bootstrap";
 
 const AddTask = () => {
   return (
-    <div>addTask</div>
-  )
-}
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Task</Form.Label>
+          <Form.Control type="text" placeholder="Enter email" />          
+        </Form.Group>
 
-export default AddTask
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" placeholder="Password" />
+        </Form.Group>        
+        <Button variant="primary" type="submit">
+          SAVE
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddTask;
