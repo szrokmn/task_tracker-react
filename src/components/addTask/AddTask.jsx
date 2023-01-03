eb3cab14e818cf055e3c136d8dad39160b60d044
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-03 18:11:26 +0300

    task state and date state added

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const AddTask = () => {
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+
   return (
     <div>
-      <Form>
+      <Form >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Task</Form.Label>
-          <Form.Control type="text" placeholder="Enter email" />          
+          <Form.Control type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Date</Form.Label>
-          <Form.Control type="date" placeholder="Password" />
-        </Form.Group>        
-        <Button variant="primary" type="submit">
+          <Form.Control type="date" onChange={(e) => setDate(e.target.value)}/>
+        </Form.Group>
+
+        <Button variant="primary w-50" type="submit">
           SAVE
         </Button>
       </Form>
